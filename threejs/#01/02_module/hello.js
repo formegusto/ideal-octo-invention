5c3ce066dd1c0616e2623cc6d29c64b3e7cb155e
formegusto <formegusto@gmail.com>
2023-03-06 01:49:09 +0900

    three js 1강 실습 자료, webpack 신기해! 나중에 깊게 배워보기

---
+++ threejs/#01/02_module/hello.js
@@ -0,0 +1,11 @@
+export function hello1() {
+  console.log("Hello One !!!!");
+}
+
+export function hello2() {
+  console.log("Hello Two !!!!");
+}
+
+export default function helloDefault() {
+  console.log("Hello Default !!!!");
+}
---
+++ threejs/#01/02_module/main.js
@@ -0,0 +1,10 @@
+// import { hello1, hello2 } from "./hello.js";
+
+// hello1();
+// hello2();
+
+import * as Hello from "./hello.js";
+
+Hello.hello1();
+Hello.hello2();
+Hello.default();
