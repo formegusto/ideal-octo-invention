d2790f6424f038cf14996e3bd71e83cb3e5007c9
formegusto <formegusto@gmail.com>
2023-03-15 17:30:49 +0900

    빛, 조명

---
+++ threejs/#02/src/ex04.js
@@ -0,0 +1,53 @@
+import * as THREE from "three";
+
+export default function example() {
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  const scene = new THREE.Scene();
+
+  // 원근 카메라(Perspective Camera)
+  const camera = new THREE.PerspectiveCamera(
+    75, // 시야각 (fov)
+    window.innerWidth / window.innerHeight, // aspect
+    0.1, // near
+    1000 // far
+  );
+  camera.position.z = 5;
+  camera.position.y = 2;
+  camera.position.x = 2;
+  scene.add(camera);
+
+  const light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.z = 2;
+  light.position.x = 1;
+  scene.add(light);
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  // const material = new THREE.MeshBasicMaterial({
+  //   color: "red",
+  // });
+  const material = new THREE.MeshStandardMaterial({
+    color: "red",
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  renderer.render(scene, camera);
+
+  // 브라우저 사이즈 변경 대응
+  function setSize() {
+    // 종횡비 수정
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix(); // orthographic에서 봤죠?
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  window.addEventListener("resize", setSize);
+}
